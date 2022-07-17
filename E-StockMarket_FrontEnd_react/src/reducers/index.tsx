340868fd5210eb23363fdf0e95351d7dcc2d6565
Gouthamamithran Raju Ashok <gouthamamithran.ashok26@outlook.com>
2022-07-17 12:24:48 +0530

    create-react-app init

--- E-StockMarket_FrontEnd_react/src/reducers/index.tsx
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import authReducer from './authReducer';
-import companyReducer from './companyReducer';
-
-export default combineReducers({
-	auth: authReducer,
-	company: companyReducer,
-});
--- E-StockMarket_FrontEnd_react/src/test/testUtils.tsx
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux';
-import reducers from '../reducers';
-
-const storeFactory = (initialState: any) => {
-	return createStore(reducers, initialState);
-};
-
-export default storeFactory;
